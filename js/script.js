6ee2c2f4c9d4a4990ddc0bdd1518d846817f8f4a
Francesco98Welcome <frankbenvenuti04@gmail.com>
2022-12-16 14:44:48 +0100

    fine primo esercizio JS

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual'è il tuo nome?");
+const cognome = prompt("Qual'è il tuo cognome?");
+const favouriteColor = prompt("Qual'è il tuo colore preferito?");
+const result = nome + cognome + favouriteColor;
+
+document.getElementById('result').innerHTML = result + 21;
